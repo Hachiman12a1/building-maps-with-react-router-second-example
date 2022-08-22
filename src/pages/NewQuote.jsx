99a9152ec5b907e1e3715b285b9ee2cbcd73b78a
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-22 17:45:39 +0700

    284. Implementing Programmatic (Imperative) Navigation

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import QuoteForm from './../components/quotes/QuoteForm';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import QuoteForm from "./../components/quotes/QuoteForm";
 
-NewQuote.propTypes = {
-    
-};
+NewQuote.propTypes = {};
 
 function NewQuote(props) {
-    const addQuoteHandler = (quoteData) => {
-        console.log(quoteData);
-    }
-    return (
-        <QuoteForm onAddQuote = {addQuoteHandler}/>
-    );
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push("/quotes");
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
 
 export default NewQuote;
