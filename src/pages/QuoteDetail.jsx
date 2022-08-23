43419e0bf8e9aa572a331de32649794dc040fff0
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-23 09:05:39 +0700

    287. Getting Creative With Nested Routes

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,8 +1,7 @@
 import React, { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
-import Comments from './../components/comments/Comments';
-import HighlightedQuote from './../components/quotes/HighlightedQuote';
-
+import { Link, Route, useParams } from "react-router-dom";
+import Comments from "./../components/comments/Comments";
+import HighlightedQuote from "./../components/quotes/HighlightedQuote";
 
 const DUMMY_DATA = [
   { id: "q1", author: "Dat", text: "Learning React is fun!" },
@@ -12,17 +11,24 @@ const DUMMY_DATA = [
 function QuoteDetail(props) {
   const params = useParams();
 
-  const quote =  DUMMY_DATA.find(quote => quote.id === params.quoteId)
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
 
-  if(!quote) {
-    return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author = {quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div>
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+        <Comments />
       </Route>
     </Fragment>
   );
